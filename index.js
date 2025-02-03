7424dba312f45bbbf4aebad6a5484f8d884e4cec
Sasikumar S <sasi071811@gmail.com>
2025-02-03 10:46:03 +0530

    db configure

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.listen('1848', ()=> console.log('server running on 1848'))
 
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://sasi071811:sasi KUMAR071811@mycluster.mr37s.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster)
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
